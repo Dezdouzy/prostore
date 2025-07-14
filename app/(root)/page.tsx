3ea7210aca7415a2f5c225e3daa1a83225916be9
Redouane Kassa <redouane@Mac.lan>
2025-07-14 16:27:40 +0200

    Video 15 Completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,14 @@
+import ProductList from "@/components/ui/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 
 const Homepage = () => {
-  return (  <>Prostore is here</>);
+  return (  <>
+              <ProductList 
+                data={sampleData.products} 
+                title="Newets Arrivals" 
+                limit={4}/>
+    </>);
 }
  
 export default Homepage;
